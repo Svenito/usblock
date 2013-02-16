f1c7e397b60e0333927c318e7edd75f763b23c90
Sven Steinbauer <sven@unlogic.co.uk>
2013-02-16 11:32:45 +0000

    Set home page URL to be github page

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='USBLock',
-    version='0.1.4',
+    version='0.1.5',
     author='Sven Steinbauer',
     author_email='sven@unlogic.co.uk',
     packages=['usblock'],
     scripts=['bin/usblock'],
-    url='http://pypi.python.org/pypi/USBLock',
+    url='https://github.com/Svenito/usblock',
     license='LICENSE.txt',
     description='Lock and unlock your desktop using a USB stick as a key.',
     long_description=open('README.txt').read(),
